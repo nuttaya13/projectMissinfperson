ffd228f9ba71c5e3683544c49ada1bc96282be84
nuttaya13 <55174262+nuttaya13@users.noreply.github.com>
2021-06-17 11:25:36 +0700

    Add files via upload

---
+++ includes/sidebar.php
@@ -0,0 +1,10 @@
+<div class="profile_nav">
+          <ul>
+            <li><a href="profile.php">โปรไฟล์</a></li>
+              <li><a href="update-password.php">เปลี่ยนรหัสผ่าน</a></li>
+            <li><a href="history-report.php">รายการเคยแจ้ง</a></li>
+            <li><a href="add.php">แจ้งรายชื่อผู้สูญหาย</a></li>
+            <li><a href="logout.php">ออกจากระบบ</a></li>
+          </ul>
+        </div>
+      </div>
